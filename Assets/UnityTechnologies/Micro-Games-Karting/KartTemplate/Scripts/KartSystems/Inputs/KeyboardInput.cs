e814b95440ce8a6f8020c8b3002b5ac040d5a2db
Jose Huillca <josehuillca@Joses-MacBook-Pro.local>
2019-09-09 08:44:01 -0300

    Builld App android

--- Assets/UnityTechnologies/Micro-Games-Karting/KartTemplate/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/UnityTechnologies/Micro-Games-Karting/KartTemplate/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -54,9 +54,11 @@ namespace KartGame.KartSystems
                 m_Acceleration = 0f;*/
             m_Acceleration = 1f;
 
-            if (Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.RightArrow))
+            //if (Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.RightArrow))
+            if (Input.acceleration.x < 0 )
                 m_Steering = -1f;
-            else if (!Input.GetKey (KeyCode.LeftArrow) && Input.GetKey (KeyCode.RightArrow))
+            //else if (!Input.GetKey (KeyCode.LeftArrow) && Input.GetKey (KeyCode.RightArrow))
+            else if (Input.acceleration.x > 0)
                 m_Steering = 1f;
             else
                 m_Steering = 0f;
